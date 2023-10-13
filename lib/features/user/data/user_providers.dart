33356cf21d13888a7efc236327053f7c78601844
Philip Johnson <johnson@hawaii.edu>
2023-10-13 09:39:55 -1000

    firestore and user providers!

--- lib/features/user/data/user_providers.dart
+++ lib/features/user/data/user_providers.dart
@@ -1,26 +1,53 @@
 import 'package:firebase_auth/firebase_auth.dart' hide User;
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../repositories/firestore/firestore_providers.dart';
 import '../domain/user.dart';
 import 'user_database.dart';
 
-final userDatabaseProvider = Provider<UserDatabase>((ref) {
-  return UserDatabase(ref);
-});
+part 'user_providers.g.dart';
 
-final usersProvider = StreamProvider<List<User>>((ref) {
+@riverpod
+UserDatabase userDatabase(UserDatabaseRef ref) {
+  return UserDatabase(ref);
+}
+
+@riverpod
+Stream<List<User>> users(UsersRef ref) {
   final database = ref.watch(userDatabaseProvider);
   return database.watchUsers();
-});
+}
 
-final currentUserIDProvider = StateProvider<String>((ref) {
+@riverpod
+String currentUserID(CurrentUserIDRef ref) {
   final FirebaseAuth instance = ref.watch(firebaseAuthProvider);
   return instance.currentUser!.email!;
-});
+}
 
-final currentUserProvider = FutureProvider<User>((ref) async {
+@riverpod
+Future<User> currentUser(CurrentUserRef ref) async {
   final String currentUserId = ref.watch(currentUserIDProvider);
   final database = ref.watch(userDatabaseProvider);
   return await database.fetchUser(currentUserId);
-});
+}
+
+// Old way:
+// final userDatabaseProvider = Provider<UserDatabase>((ref) {
+//   return UserDatabase(ref);
+// });
+//
+// final usersProvider = StreamProvider<List<User>>((ref) {
+//   final database = ref.watch(userDatabaseProvider);
+//   return database.watchUsers();
+// });
+//
+// final currentUserIDProvider = StateProvider<String>((ref) {
+//   final FirebaseAuth instance = ref.watch(firebaseAuthProvider);
+//   return instance.currentUser!.email!;
+// });
+//
+// final currentUserProvider = FutureProvider<User>((ref) async {
+//   final String currentUserId = ref.watch(currentUserIDProvider);
+//   final database = ref.watch(userDatabaseProvider);
+//   return await database.fetchUser(currentUserId);
+// });
